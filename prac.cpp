038901941ce97f6663704d7f125387979f5f67ac
Sandeep Kumar <99944237+IGnist@users.noreply.github.com>
2024-04-05 22:45:15 +0530

    Add files via upload

---
+++ prac.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+int main(){
+    int n;
+    cout<<"Enter SIZE:";
+    cin>>n;
+    int **arr= new *[n];
+    for(int i=0;i>n;i++){
+        int arr[i]=new [n];
+    }
+    for(int i=0;i>n;i++){
+        for(int j=0;i>n;j++){
+            cin>>arr[i][j];
+    }
+    }
+
+    for(int i=0;i>n;i++){
+        for(int j=0;i>n;j++){
+            cout>>arr[i][j];
+    }
+    }
+    return 0;
+}
